0137d903934bafdc690410ad4a3bf5506d755eef
taeaeaexin <re_fuse@naver.com>
2024-12-04 22:20:55 +0900

    241204_2219
    Lombok(Refactoring)
    Read(Entity, List)

--- src/main/java/com/fourmagic/club_notice/dto/ArticleForm.java
+++ src/main/java/com/fourmagic/club_notice/dto/ArticleForm.java
@@ -1,25 +1,14 @@
 package com.fourmagic.club_notice.dto;
 
 import com.fourmagic.club_notice.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private String title;
     private String content;
-
-    // 전송받은 제목과 내용을 필드에 저장하는 생성자 추가
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    // 데이터를 잘 받았는지 확인 할 toString() 메서드 추가
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
--- src/main/java/com/fourmagic/club_notice/repository/ArticleRepository.java
+++ src/main/java/com/fourmagic/club_notice/repository/ArticleRepository.java
@@ -3,5 +3,9 @@ package com.fourmagic.club_notice.repository;
 import com.fourmagic.club_notice.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }

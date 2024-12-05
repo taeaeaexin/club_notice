aba404a3df9c585043ffcc1c33062c7f0287e190
taeaeaexin <re_fuse@naver.com>
2024-12-05 12:54:48 +0900

    241205_1254
    Link, Redirect

--- src/main/java/com/fourmagic/club_notice/entity/Article.java
+++ src/main/java/com/fourmagic/club_notice/entity/Article.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
 @ToString
 @Entity
+@Getter
 @NoArgsConstructor // 기본 생성자 추가 어노테이션
 public class Article {
     @Id
